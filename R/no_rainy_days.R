113eb463de495f4f6ed6cc1cb3136c2ea69963d0
Tom Higginbottom <T.Higginbottom@mmu.ac.uk>
2016-12-12 10:56:29 +0000

    Initial Upload

---
+++ R/no_rainy_days.R
@@ -0,0 +1,51 @@
+#' Number of rainy days
+#'
+#' Calculate number of days where rainfall (above a threshold) occurs
+#' 
+#' @param daily_rfe Folder containing the downloaded daily-rfe from tamsat_daily_download
+#' @param start_doy Day of Year (DDD-format) for the start of the period
+#' @param end_doy Day of Year (DDD-format) for the end of the period
+#' @param threshold threshold for classifying a rainy day, in mm/day, defaults to zero is missing
+#' @param years Years as interger(s) for which statistics will be calculated
+#' @param id Charecter sting to identify the outputs eg "long_integer/ jan_march etc"
+#' @param outlocation Folder (string) to save summary layers into
+#
+
+
+no_rainy_days <- function(daily_rfe, years, start_doy, threshold, id, outlocation){
+  
+  if(missing(threshold)==TRUE){threshold = 0}
+  
+  listFiles <- list.files(path = daily_rfe,pattern = ".nc",recursive = TRUE,full.names = TRUE)
+  for (i in years){
+    print(i)
+    start_date <- as.Date(start_doy, origin = paste0(i,"-01-01"))
+    start_date <- as.Date(start_date,format="%Y/%m/%d")
+    start_date <- gsub(pattern = "-","_",start_date)
+    start_grep <- paste0("rfe",start_date)
+    
+    end_doy <- start_doy-2
+    end_date <- as.Date(end_doy, origin = paste0(i+1,"-01-01"))
+    end_date <- as.Date(end_date,format="%Y/%m/%d")
+    end_date <- gsub(pattern = "-","_",end_date)
+    end_grep <- paste0("rfe",end_date)
+    
+    
+    startPos <- grep(pattern = start_grep,x = listFiles)
+    endPos <- grep(pattern = end_grep,x = listFiles)
+    
+    year_stack <-   raster::brick(sapply(listFiles[startPos:endPos], FUN = raster::raster))
+    
+    flname <- paste0(outlocation,"/", id, "rainy_days_", i,".tif" )
+      
+    # count wet days 
+    raster::calc(year_stack, function(x) sum(x > threshold),filename = flname) 
+
+  }}
+
+
+
+
+
+
+
---
+++ R/tamsat_daily_download.R
@@ -0,0 +1,31 @@
+#' Download Tamsat Daily Rainfall Estimate
+#'
+#' Automate downloading of daily RFE's from the TAMSAT server
+#' Note that the unZip utilities within R are particually slow
+#' 
+#' 
+#' @param years Years as interger(s) for which data will be downloaded
+#' @param outlocation Folder (string) to save downloaded .zip files into
+#' @param unZip Should the downloaded zips be unzipped
+#
+
+tamsat_daily_download <- function(years,outlocation,unZip){
+ 
+  for (i in years){
+    filePath<-  paste0("http://www.tamsat.org.uk/public_data/", i, "/TAMSAT_rfe_daily",i, ".zip")
+    destPath <- paste0(outlocation, basename(filePath))
+    print("downloading year-", i)
+    curl::curl_download(url = filePath,destfile =destPath,mode="wb")
+  }
+  
+if(unZip==TRUE){
+  for (i in list.files(pattern = ".zip",path = outlocation)){
+    
+    unzip_folder <- paste0(outlocation,"/",file_path_sans_ext(i))
+    print("Un-ziping to folder-", unzip_folder)
+    
+        unzip(zipfile = i,exdir =unzip_folder, overwrite = TRUE)
+        file.remove(i)
+}}}
+
+
