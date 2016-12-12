fb340e5176a0ae91c8be940ebe5af723d4e7e683
Tom Higginbottom <T.Higginbottom@mmu.ac.uk>
2016-12-12 12:03:49 +0000

    second initial upload

--- R/annual_summary.R
+++ R/annual_summary.R
@@ -1,10 +1,8 @@
-#' Annual Summary Statistics 
+#' Annual; Summary Statistics 
 #'
-#' Calculate statistical summaries of rainfall over a defined period
+#' Calculate statistical summaries of rainfall over a calender year 
 #' 
 #' @param daily_rfe Folder containing the downloaded daily-rfe from tamsat_daily_download
-#' @param start_doy Day of Year (DDD-format) for the start of the period
-#' @param end_doy Day of Year (DDD-format) for the end of the period
 #' @param stat statistic to calculate, must be compatible with raster::calc()
 #' @param years Years as interger(s) for which statistics will be calculated
 #' @param id Charecter sting to identify the outputs eg "long_integer/ jan_march etc"
@@ -12,32 +10,17 @@
 #
 
 
-annual_summary   <- function(daily_rfe, years, start_doy, end_doy, stat, id, outlocation){
+annual_summary   <- function(daily_rfe, years, stat, id, outlocation){
   
   listFiles <- list.files(path = daily_rfe, pattern = ".nc", recursive = TRUE, full.names = TRUE)
   for (i in years){
-  print(i)
-    start_date <- as.Date(start_doy, origin = paste0(i,"-01-01"))
-    start_date <- as.Date(start_date,format="%Y/%m/%d")
-    start_date <- gsub(pattern = "-","_",start_date)
-    start_grep <- paste0("rfe",start_date)
+    print(i)
     
-    if(start_doy < start_doy){i = i+1}
+    year_list <- grep(pattern = i,x = listFiles,value = TRUE)
+
+    year_stack <-   raster::stack(sapply(year_list, FUN = raster::raster))
     
-    end_doy <- start_doy-2
-    end_date <- as.Date(end_doy, origin = paste0(i+1,"-01-01"))
-    end_date <- as.Date(end_date,format="%Y/%m/%d")
-    end_date <- gsub(pattern = "-","_",end_date)
-    end_grep <- paste0("rfe",end_date)
+    flname <- paste0(outlocation,"/", id,"_",stat, i,".tif")
     
-    
-    startPos <- grep(pattern = start_grep,x = listFiles)
-    endPos <- grep(pattern = end_grep,x = listFiles)
-    
-    year_stack <-   raster::stack(sapply(listFiles[startPos:endPos], FUN = raster::raster))
-    
-    flname <- paste0(outlocation,"/",id,"_",stat, i,".tif" )
-    
-    raster::calc(x = year_stack,fun = sum, filename = flname  )
+    raster::calc(x = year_stack,fun = sum, filename = flname)
   }}
-  
--- R/window_summary.R
+++ R/window_summary.R
@@ -1,31 +1,30 @@
-#' Number of rainy days
+#' Window Summary Statistics 
 #'
-#' Calculate number of days where rainfall (above a threshold) occurs
+#' Calculate statistical summaries of rainfall over a defined period
 #' 
 #' @param daily_rfe Folder containing the downloaded daily-rfe from tamsat_daily_download
-#' @param start_doy Day of Year (DDD-format) for the start of the period
+#' @param start_doy Day of Year (DDD-format) for the start of the period 
 #' @param end_doy Day of Year (DDD-format) for the end of the period
-#' @param threshold threshold for classifying a rainy day, in mm/day, defaults to zero is missing
+#' @param stat statistic to calculate, must be compatible with raster::calc()
 #' @param years Years as interger(s) for which statistics will be calculated
 #' @param id Charecter sting to identify the outputs eg "long_integer/ jan_march etc"
 #' @param outlocation Folder (string) to save summary layers into
 #
 
 
-no_rainy_days <- function(daily_rfe, years, start_doy, threshold, id, outlocation){
+window_summary   <- function(daily_rfe, years, start_doy, end_doy, stat, id, outlocation){
   
-  if(missing(threshold)==TRUE){threshold = 0}
-  
-  listFiles <- list.files(path = daily_rfe,pattern = ".nc",recursive = TRUE,full.names = TRUE)
+  listFiles <- list.files(path = daily_rfe, pattern = ".nc", recursive = TRUE, full.names = TRUE)
   for (i in years){
-    print(i)
+  print(i)
     start_date <- as.Date(start_doy, origin = paste0(i,"-01-01"))
     start_date <- as.Date(start_date,format="%Y/%m/%d")
     start_date <- gsub(pattern = "-","_",start_date)
     start_grep <- paste0("rfe",start_date)
     
-    end_doy <- start_doy-2
-    end_date <- as.Date(end_doy, origin = paste0(i+1,"-01-01"))
+    if(end_doy < start_doy)
+      {end_date <- as.Date(end_doy, origin = paste0(i+1, "-01-01"))}
+    
     end_date <- as.Date(end_date,format="%Y/%m/%d")
     end_date <- gsub(pattern = "-","_",end_date)
     end_grep <- paste0("rfe",end_date)
@@ -34,18 +33,10 @@ no_rainy_days <- function(daily_rfe, years, start_doy, threshold, id, outlocatio
     startPos <- grep(pattern = start_grep,x = listFiles)
     endPos <- grep(pattern = end_grep,x = listFiles)
     
-    year_stack <-   raster::brick(sapply(listFiles[startPos:endPos], FUN = raster::raster))
+    year_stack <-   raster::stack(sapply(listFiles[startPos:endPos], FUN = raster::raster))
     
-    flname <- paste0(outlocation,"/", id, "_", "rainy_days_", i,".tif" )
-      
-    # count wet days 
-    raster::calc(year_stack, function(x) sum(x > threshold),filename = flname) 
-
+    flname <- paste0(outlocation,"/", id,"_",stat, i,".tif")
+    
+    raster::calc(x = year_stack,fun = sum, filename = flname)
   }}
-
-
-
-
-
-
-
+  
